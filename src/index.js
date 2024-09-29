385071be020eb7209c2c2fc5e6efa3afb429ad7d
Sainath Shinde <shindesainath692@gmail.com>
2024-09-29 16:53:11 +0530

    utils code for apierror , apiresponse, asynchandler

--- src/index.js
+++ src/index.js
@@ -5,7 +5,21 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERRR", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("ERRR", error);
+    throw error;
+  });
 
 /*  1st Approach of DB connection
 (async () => {
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+//Higher oreder function as Wraper function for the code base
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// -------------------------------------------------------
+
+/* const asyncHandler = () => {};
+const asyncHandler = (func) => () => {};
+const asyncHandler = (func) => async () => {}; */
+
+//Higher oreder function as Wraper function for the code base
+
+/* const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(err.code || 500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+}; */
